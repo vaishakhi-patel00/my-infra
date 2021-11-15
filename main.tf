80250617ba486a66c203a58c14ebb0c55e125298
Vaishakhi Patel <Vaishakhi.Patel@everestek.com>
2021-11-15 12:47:25 +0530

    Managing infrastructure as code!

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  
+  ami           = "ami-04ad2567c9e3d7893"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+    id="vaishakhi"
+  }
+}
+
